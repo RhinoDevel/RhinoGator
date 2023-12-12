ef3fe06ec4aba4660c27e76efdb612735398f8d0
Marc <RhinoDevel@users.noreply.github.com>
2023-12-12 13:23:08 +0100

    Clock test code.

--- RhinoGator/Program.cs
+++ RhinoGator/Program.cs
@@ -1,4 +1,21 @@
 ﻿
 // RhinoDevel, MT, 2023dec12
 
+using Ele;
+
+// F = 10000 Hz <=> T = 1/10000 s = 100000 ns
+
+var clock = new Clock(
+        new ClockParams
+        {
+            PulseSteps = 10000,
+            StartHigh = false
+        });
+
+clock.Forward(0);
+clock.Forward(9999);
+clock.Forward(10000);
+clock.Forward(19999);
+clock.Forward(20000);
+
 Console.WriteLine("RhinoGator!");
