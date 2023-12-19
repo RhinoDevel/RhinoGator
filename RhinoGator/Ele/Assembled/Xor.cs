cf23c7cd7b5e832d61caf39892aade76fadc68b2
Marc <RhinoDevel@users.noreply.github.com>
2023-12-19 12:33:59 +0100

    XOR element.

---
+++ RhinoGator/Ele/Assembled/Xor.cs
@@ -0,0 +1,40 @@
+
+// RhinoDevel, MT, 2023dec19
+
+namespace RhinoGator.Ele.Assembled
+{
+    /// <remarks>
+    /// Page 37.
+    /// </remarks>
+    internal class Xor
+    {
+        private Basic.Not _notA = new Basic.Not();
+        private Basic.Not _notB = new Basic.Not();
+        private Basic.And _andA = new Basic.And();
+        private Basic.And _andB = new Basic.And();
+        private Basic.Or _or = new Basic.Or();
+
+        internal State Output
+        { 
+            get
+            {
+                return _or.Output;
+            }
+            private set
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public void Update(State inputA, State inputB)
+        {
+            _notA.Update(inputA);
+            _andA.Update(new List<State>{ _notA.Output, inputB });
+
+            _notB.Update(inputB);
+            _andB.Update(new List<State>{ _notB.Output, inputA });
+
+            _or.Update(new List<State>{ _andA.Output, _andB.Output });
+        }
+    }
+}
