cb88f02b3640645505223e1e4ea345f2680767a7
Marc <RhinoDevel@users.noreply.github.com>
2023-12-20 21:27:49 +0100

    Improve game loop.

--- RhinoGator/GameLoop.cs
+++ RhinoGator/GameLoop.cs
@@ -1,6 +1,8 @@
 
 // Marcel Timm, RhinoDevel, 2023dec20
 
+using System.Diagnostics;
+
 namespace RhinoGator
 {
     internal static class GameLoop
@@ -8,15 +10,13 @@ namespace RhinoGator
         private const byte _consoleWidth = 80;
         private const byte _consoleHeight = 25;
 
-        private const int _freq = 60; // Hz
-        private const int _ms = 
-            (int)(1000.0 * 1.0 / (double)_freq + 0.5); // Rounds
+        private const int _freq = 25; // Hz / FPS.
+        private const double _ms = 1000.0 * 1.0 / (double)_freq;
+        private const long _ticks = (long)(10.0 * 1000.0 * _ms + 0.5); // Rounds
 
-        private static void BlitToConsole(int left, int top)
+        private static void BlitToConsole()
         {
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.SetCursorPosition(left, top);
-            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
 
             for(int row = 0; row < _consoleHeight; ++row)
             {
@@ -32,23 +32,28 @@ namespace RhinoGator
 
         internal static void Start()
         {
-            int top = -1;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.CursorVisible = false;
 
-            // BAD: Just expecting the delay to be exact (no expl. game time)!
-            //
             while (true)
             {
+                var beginTicks = DateTime.Now.Ticks;
+
                 // TODO: Call input handling routine!
                 
                 // TODO: Call (graphical) output updating/creating routine!
 
-                if(top == -1) // To keep (debug) output above graphics.
-                {
-                    top = Console.CursorTop;
-                }
-                BlitToConsole(0, top);
+                BlitToConsole();
 
-                Thread.Sleep(_ms);
+                var endTicks = DateTime.Now.Ticks;
+
+                var elapsedTicks = endTicks - beginTicks;
+
+                Debug.Assert(_ticks >= elapsedTicks);
+
+                var leftTicks = _ticks - elapsedTicks;
+
+                Thread.Sleep(new TimeSpan(leftTicks));
             }
         }
     }
