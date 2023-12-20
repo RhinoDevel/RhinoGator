c6f0e10c29553051379fd72953350bf4d4f19b2e
Marc <RhinoDevel@users.noreply.github.com>
2023-12-20 10:47:11 +0100

    Default helper method to get specific output state.

--- RhinoGator/Ele/Basic/Base.cs
+++ RhinoGator/Ele/Basic/Base.cs
@@ -6,5 +6,74 @@ namespace RhinoGator.Ele.Basic
     internal abstract class Base
     {
         internal State Output { get; private protected set; } = State.Unknown;
+
+        /// <summary>
+        /// Get the output (state) that should follow based on the last output
+        /// and the logically next output, only.
+        /// </summary>
+        /// <param name="lastOutput">
+        /// The last (most recent, latest) output of this element.
+        /// </param>
+        /// <param name="nextIsHigh">
+        /// If the next output should logically be high (otherwise it should
+        /// logically be low).
+        /// </param>
+        private protected static State GetOutput(
+            State lastOutput, bool nextIsHigh)
+        {
+            switch(lastOutput)
+            {
+                case State.Unknown:
+                {
+                    if(nextIsHigh)
+                    {
+                        return State.Rising;
+                    }
+                    return State.Low;
+                }
+
+                case State.Low:
+                {
+                    if(nextIsHigh)
+                    {
+                        return State.Rising;
+                    }
+                    return State.Low; // (keep low output)
+                }
+
+                case State.Rising:
+                {
+                    if(nextIsHigh)
+                    {
+                        return State.High;
+                    }
+                    return State.Falling;
+                }
+
+                case State.High:
+                {
+                    if(nextIsHigh)
+                    {
+                        return State.High; // (keep high output)
+                    }
+                    return State.Falling;
+                }
+
+                case State.Falling:
+                {
+                    if(nextIsHigh)
+                    {
+                        return State.Rising;
+                    }
+                    return State.Low;
+                }
+
+                default:
+                {
+                    throw new Exception(
+                        $"Unsupported (last) output state {(int)lastOutput}!");
+                }
+            }
+        }
     }
 }
