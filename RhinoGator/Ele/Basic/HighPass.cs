e171a349f0283180d316db8ad65260ac720f8e5d
Marc <RhinoDevel@users.noreply.github.com>
2023-12-20 11:04:21 +0100

    Use default get-output method for high pass element, too.

--- RhinoGator/Ele/Basic/HighPass.cs
+++ RhinoGator/Ele/Basic/HighPass.cs
@@ -8,87 +8,13 @@ namespace RhinoGator.Ele.Basic
     /// <summary>
     /// High-pass filter.
     /// </summary>
-    /// <remarks>
-    /// Output rises (under some circumstances), if input is high.
-    /// Output gets high, if input is still high and output has risen.
-    /// Output gets low after being high.
-    /// </remarks>
     internal class HighPass : Base
     {
         internal void Update(State input)
         {
-            switch(Output)
-            {
-                case State.Unknown:
-                {
-                    // On "init", rise, if input is high,
-                    // otherwise set output to defined low state:
+            bool nextIsHigh = Output != State.High && input == State.High;
 
-                    if(input == State.High)
-                    {
-                        Output = State.Rising;
-                        return;
-                    }
-                    Output = State.Low;
-                    return;
-                }
-
-                case State.Low:
-                {
-                    // Rise, if input is high, stay low otherwise:
-
-                    if(input == State.High)
-                    {
-                        Output = State.Rising;
-                        return;
-                    }
-                    return; // (keep low output)
-                }
-
-                case State.Rising:
-                {
-                    // If input is still high, set output to high,
-                    // otherwise let output fall:
-
-                    if(input == State.High)
-                    {
-                        Output = State.High;
-                        return;
-                    }
-                    Output = State.Falling;
-                    return;
-                }
-
-                case State.High:
-                {
-                    // The high state is short, because this is a high-pass
-                    // filter:
-
-                    Output = State.Falling;
-                    return;
-                }
-
-                case State.Falling:
-                {
-                    // Rise (again), if input got (suddenly..) high,
-                    // otherwise reach low state:
-
-                    if(input == State.High)
-                    {
-                        Debug.Assert(false); // Should not happen (no rise?)..
-                        Output = State.Rising;
-                        return;
-                    }
-                    Output = State.Low;
-                    return;
-                }
-
-                default:
-                {
-                    throw new Exception(
-                        $"Unsupported (last) output state {(int)Output}!");
-                }
-            }
+            Output = GetOutput(Output, nextIsHigh);
         }
     }
 }
