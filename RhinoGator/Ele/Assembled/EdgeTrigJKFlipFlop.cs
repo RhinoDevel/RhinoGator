a933feccf65f7b7fcc60554ace1672269f9c47ac
Marc <RhinoDevel@users.noreply.github.com>
2023-12-25 18:47:36 +0100

    Currently NOT working edge-triggered JK flip-flop with example.

---
+++ RhinoGator/Ele/Assembled/EdgeTrigJKFlipFlop.cs
@@ -0,0 +1,63 @@
+
+// RhinoDevel, MT, 2023dec25
+
+using RhinoGator.Ele.Basic;
+
+namespace RhinoGator.Ele.Assembled
+{
+    /// <remarks>
+    /// Page 99.
+    /// 
+    /// Not direct-coupled.
+    /// </remarks>
+    internal class EdgeTrigJKFlipFlop : Base
+    {
+        private HighPass _hp = new HighPass(6);
+        private Nand _nandR = new Nand();
+        private Nand _nandS = new Nand();
+        private RsNandLatch _latch = new RsNandLatch();
+
+        internal State Output
+        { 
+            get
+            {
+                return _latch.Output;
+            }
+            private set
+            {
+                throw new NotSupportedException();
+            }
+        }
+        internal State SecondOutput
+        { 
+            get
+            {
+                return _latch.SecondOutput;
+            }
+            private set
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        internal override OutputDep GetDependencies()
+        {
+            return _hp.Dependencies
+                | _nandR.GetDependencies()
+                | _nandS.GetDependencies()
+                | _latch.GetDependencies();
+        }
+
+        internal void Update(State j, State k, State clk)
+        {
+            _hp.Update(new List<State>{ clk });
+
+            _nandR.Update(
+                new List<State> { j, _hp.Output, _latch.SecondOutput });
+
+            _nandS.Update(new List<State> { k, _hp.Output, _latch.Output });
+
+            _latch.Update(_nandR.Output, _nandS.Output);
+        }
+    }
+}
---
+++ RhinoGator/Examples/ExampleEdgeTrigJKFlipFlop.cs
@@ -0,0 +1,60 @@
+
+// Marcel Timm, RhinoDevel, 2023dec25
+
+using RhinoGator.Ele.Assembled;
+using RhinoGator.Ele.Basic;
+using RhinoGator.Ele.Basic.Clock;
+
+namespace RhinoGator.Examples
+{
+    internal class ExampleEdgeTripJKFlipFlop : IGameLoop
+    {
+        private readonly ToggleSwitch _tsJ = new ToggleSwitch(true, true);
+        private readonly ToggleSwitch _tsK = new ToggleSwitch(true, true);
+        private readonly Clock _clock = new Clock(
+            new ClockParams
+            {
+                PulseSteps = 16,
+                StartHigh = true
+            });
+        private readonly EdgeTrigJKFlipFlop _flipFlop =
+            new EdgeTrigJKFlipFlop();
+
+        void IGameLoop.Init(int w, int h, byte[] frameBuf)
+        {
+            for(int i = 0; i < frameBuf.Length; ++i)
+            {
+                frameBuf[i] = (byte)' ';
+            }
+        }
+
+        void IGameLoop.HandleUserInput(List<ConsoleKey> pressedKeys)
+        {
+            if(pressedKeys.Contains(ConsoleKey.J))
+            {
+                _tsJ.Toggle();
+            }
+            if(pressedKeys.Contains(ConsoleKey.K))
+            {
+                _tsK.Toggle();
+            }
+        }
+
+        void IGameLoop.Update(int steps, int w, int h, byte[] frameBuf)
+        {
+            for(int i = 0;i < steps; ++i)
+            {
+                _tsJ.Update(new List<State>{ State.Low });
+                _tsK.Update(new List<State>{ State.Low });
+                _clock.Update(new List<State>());
+                _flipFlop.Update(_tsJ.Output, _tsK.Output, _clock.Output);
+
+                FrameBuf.PushStateToRow(_clock.Output, 0, w, frameBuf);
+                FrameBuf.PushStateToRow(_tsJ.Output, 2, w, frameBuf);
+                FrameBuf.PushStateToRow(_tsK.Output, 4, w, frameBuf);
+                FrameBuf.PushStateToRow(_flipFlop.Output, 6, w, frameBuf);
+                //FrameBuf.PushStateToRow(_flipFlop.SecondOutput, 8, w, frameBuf);
+            }
+        }
+    }
+}
