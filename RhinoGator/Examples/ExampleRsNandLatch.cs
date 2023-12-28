3f6d59eac9cbd20d67af7841b12070acb67b40f5
Marc <RhinoDevel@users.noreply.github.com>
2023-12-28 19:59:16 +0100

    Add preset and clear to NAND RS latch example.

--- RhinoGator/Examples/ExampleRsNandLatch.cs
+++ RhinoGator/Examples/ExampleRsNandLatch.cs
@@ -10,6 +10,8 @@ namespace RhinoGator.Examples
     {
         private readonly ToggleSwitch _tsR = new ToggleSwitch(true, false);
         private readonly ToggleSwitch _tsS = new ToggleSwitch(true, false);
+        private readonly ToggleSwitch _tsP = new ToggleSwitch(true, false);
+        private readonly ToggleSwitch _tsC = new ToggleSwitch(true, false);
         private readonly RsNandLatch _latch = new RsNandLatch();
 
         void IGameLoop.Init(int w, int h, byte[] frameBuf)
@@ -30,21 +32,33 @@ namespace RhinoGator.Examples
             {
                 _tsS.Toggle();
             }
+            if(pressedKeys.Contains(ConsoleKey.P))
+            {
+                _tsP.Toggle();
+            }
+            if(pressedKeys.Contains(ConsoleKey.C))
+            {
+                _tsC.Toggle();
+            }
         }
 
         void IGameLoop.Update(int steps, int w, int h, byte[] frameBuf)
         {
             _tsR.Update(new List<State>{ State.Low });
             _tsS.Update(new List<State>{ State.Low });
-            _latch.Update(_tsR.Output, _tsS.Output);
+            _tsP.Update(new List<State>{ State.Low });
+            _tsC.Update(new List<State>{ State.Low });
+            _latch.Update(_tsR.Output, _tsS.Output, _tsP.Output, _tsC.Output);
             
-            for(int i = 0;i < steps; ++i)
+            for(int i = 0; i < steps; ++i)
             {
                 FrameBuf.PushStateToRow('R', _tsR.Output, 0, w, frameBuf);
                 FrameBuf.PushStateToRow('S', _tsS.Output, 2, w, frameBuf);
-                FrameBuf.PushStateToRow('Q', _latch.Output, 4, w, frameBuf);
+                FrameBuf.PushStateToRow('p', _tsP.Output, 4, w, frameBuf);
+                FrameBuf.PushStateToRow('c', _tsC.Output, 6, w, frameBuf);
+                FrameBuf.PushStateToRow('Q', _latch.Output, 8, w, frameBuf);
                 FrameBuf.PushStateToRow(
-                    'q', _latch.SecondOutput, 6, w, frameBuf);
+                    'q', _latch.SecondOutput, 10, w, frameBuf);
             }
         }
     }
