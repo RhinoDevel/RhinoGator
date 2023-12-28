8e1f14207af1a1313bf9ed68524934c853cbf872
Marc <RhinoDevel@users.noreply.github.com>
2023-12-28 21:41:28 +0100

    Ripple counter example.

---
+++ RhinoGator/Examples/ExampleRippleCounter.cs
@@ -0,0 +1,63 @@
+
+// Marcel Timm, RhinoDevel, 2023dec28
+
+using System.Diagnostics;
+using RhinoGator.Ele.Assembled;
+using RhinoGator.Ele.Basic;
+using RhinoGator.Ele.Basic.Clock;
+
+namespace RhinoGator.Examples
+{
+    internal class ExampleRippleCounter : IGameLoop
+    {
+        private readonly ToggleSwitch _tsC = new ToggleSwitch(true, false);
+        private readonly Clock _clock = new Clock(
+            new ClockParams
+            {
+                StartHigh = false,
+                PulseSteps = 2
+            });
+        private readonly RippleCounter _counter = new RippleCounter(4);
+
+        void IGameLoop.Init(int w, int h, byte[] frameBuf)
+        {
+            for(int i = 0; i < frameBuf.Length; ++i)
+            {
+                frameBuf[i] = (byte)' ';
+            }
+        }
+
+        void IGameLoop.HandleUserInput(List<ConsoleKey> pressedKeys)
+        {
+            if(pressedKeys.Contains(ConsoleKey.C))
+            {
+                _tsC.Toggle();
+            }
+        }
+
+        void IGameLoop.Update(int steps, int w, int h, byte[] frameBuf)
+        {   
+            _tsC.Update(new List<State>{ State.Low });
+
+            for(int i = 0; i < steps; ++i)
+            {
+                List<State> counterOutputs;
+
+                _clock.Update(new List<State>());
+                _counter.Update(_clock.Output, _tsC.Output);
+
+                FrameBuf.PushStateToRow('c', _tsC.Output, 0, w, frameBuf);
+                FrameBuf.PushStateToRow('C', _clock.Output, 2, w, frameBuf);
+
+                counterOutputs = _counter.Outputs;
+                Debug.Assert(counterOutputs.Count == 4);
+                FrameBuf.PushStateToRow('0', counterOutputs[0], 4, w, frameBuf);
+                FrameBuf.PushStateToRow('1', counterOutputs[1], 6, w, frameBuf);
+                FrameBuf.PushStateToRow('2', counterOutputs[2], 8, w, frameBuf);
+                FrameBuf.PushStateToRow(
+                    '3', counterOutputs[3], 10, w, frameBuf);
+
+            }
+        }
+    }
+}
