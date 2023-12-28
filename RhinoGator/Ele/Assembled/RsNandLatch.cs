3c3a9584e9a3e49ac8c1bd2f6772ab032fbb98c3
Marc <RhinoDevel@users.noreply.github.com>
2023-12-28 19:58:55 +0100

    Add "secure" preset and clear inputs to NAND RS latch.

--- RhinoGator/Ele/Assembled/RsNandLatch.cs
+++ RhinoGator/Ele/Assembled/RsNandLatch.cs
@@ -1,7 +1,7 @@
 
 // RhinoDevel, MT, 2023dec22
 
-using System.Diagnostics;
+using RhinoGator.Ele.Basic;
 
 namespace RhinoGator.Ele.Assembled
 {
@@ -14,9 +14,19 @@ namespace RhinoGator.Ele.Assembled
     /// 0 1 | 1    | Set.
     /// 1 0 | 0    | Reset.
     /// 1 1 | N.C. | No change.
+    /// 
+    /// With optional preset and clear inputs (not in truth table above..).
     /// </remarks>
     internal class RsNandLatch : Base
     {
+        private And _andR = new And();
+        private Not _notR = new Not();
+        private Or _orR = new Or();
+
+        private And _andS = new And();
+        private Not _notS = new Not();
+        private Or _orS = new Or();
+
         private Nand _nandR = new Nand();
         private Nand _nandS = new Nand();
         internal State Output
@@ -44,15 +54,44 @@ namespace RhinoGator.Ele.Assembled
 
         internal override OutputDep GetDependencies()
         {
-            return _nandR.GetDependencies()
+            return _andS.Dependencies
+                | _notS.Dependencies
+                | _orS.Dependencies
+
+                | _andR.Dependencies
+                | _notR.Dependencies
+                | _orR.Dependencies
+
+                |_nandR.GetDependencies()
                 | _nandS.GetDependencies()
                 | OutputDep.LastOutputs;
         }
 
-        internal void Update(State inputR, State inputS)
+        internal void Update(
+            State inputR,
+            State inputS,
+            State negPreset = State.High,
+            State negClear = State.High)
         {
-            _nandS.Update(new List<State>{ inputS, _nandR.Output });
-            _nandR.Update(new List<State>{ inputR, _nandS.Output });
+            // **************************************************************
+            // *** Prevent race condition by never setting actual R and S ***
+            // *** both to zero, if preset or clear input is low:         ***
+            // **************************************************************
+
+            _andS.Update(new List<State> { inputS, negClear });
+            _notS.Update(new List<State> { negPreset });
+            _orS.Update(new List<State> { _andS.Output, _notS.Output });
+
+            _andR.Update(new List<State> { inputR, negPreset });
+            _notR.Update(new List<State> { negClear });
+            _orR.Update(new List<State> { _andR.Output, _notR.Output });
+
+            // *********************
+            // *** Actual latch: ***
+            // *********************
+
+            _nandS.Update(new List<State>{ _orS.Output, _nandR.Output });
+            _nandR.Update(new List<State>{ _orR.Output, _nandS.Output });
         }
     }
 }
